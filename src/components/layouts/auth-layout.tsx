54d175461389ba6cb00d92725fd1bd5fc88a6839
judricalderon <judricalderon@gmail.com>
2026-04-01 11:33:49 -0500

    no ver loguin por usuario layaut auth

--- src/components/layouts/auth-layout.tsx
+++ src/components/layouts/auth-layout.tsx
@@ -1,6 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+
+import useAuthStore from '@/stores/auth.store'
 
 const AuthLayout = () => {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
+
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" replace />
+  }
+
   return (
     <div className="bg-background flex min-h-screen items-center justify-center p-4">
       <Outlet />
